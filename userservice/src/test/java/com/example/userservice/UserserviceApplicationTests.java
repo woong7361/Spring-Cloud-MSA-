0861c6570fde3514b81853a22bc57342cb11fead
woong <woong7361@gmail.com>
2024-06-04 14:14:21 +0900

    first

---
+++ userservice/src/test/java/com/example/userservice/UserserviceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootTest
+@EnableDiscoveryClient
+class UserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
