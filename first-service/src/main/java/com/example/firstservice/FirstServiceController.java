b7d085b849e71fd4efcf8e57507f0f39339e2c1a
woong <woong7361@gmail.com>
2024-06-05 11:00:07 +0900

    loadbalencing gateway

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,6 +1,10 @@
 package com.example.firstservice;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/first-service")
 @Slf4j
+@RequiredArgsConstructor
 public class FirstServiceController {
+
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -24,8 +31,10 @@ public class FirstServiceController {
 
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi there, message check from first service";
+    public String check(HttpServletRequest request) {
+        log.info("server.port = {}", request.getServerPort());
+
+        return String.format("Hi there, message check from first service on Port %s", env.getProperty("local.server.port"));
     }
 
 
