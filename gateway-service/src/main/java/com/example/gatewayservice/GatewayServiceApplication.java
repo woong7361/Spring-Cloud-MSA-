f2607600621ad961b7403ce82828ce1740b9eddf
woong <woong7361@gmail.com>
2024-06-07 16:41:19 +0900

    local git properties

--- gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -1,6 +1,8 @@
 package com.example.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
@@ -9,6 +11,11 @@ public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	public HttpExchangeRepository httpTraceRepository() {
+		return new InMemoryHttpExchangeRepository();
 	}
 
 }
